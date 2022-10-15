2d541ceef4c8d961ddff019e60f3da04aaa2c7aa
qvd808 <dqvinh808@gmail.com>
2022-10-15 14:47:41 -0700

    Update base derek

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import HomeScreen from "./components/HomeScreen";
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import Task from './components/Task'
+import TaskPage from "./components/TaskPage";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -10,7 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
-        <Stack.Screen name="Task" options={{ headerShown: false }} component={Task} />
+        <Stack.Screen name="Task" options={{ headerShown: false }} component={TaskPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
